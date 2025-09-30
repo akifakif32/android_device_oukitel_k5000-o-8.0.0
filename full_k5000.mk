74caf1a2758a7c0f7c1c44677530ebd3c783494b
akifakif32 <mhmtkfdrn@gmail.com>
2025-09-30 21:31:37 +0300

    Initial commit for device tree

---
+++ full_k5000.mk
@@ -0,0 +1,25 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from hardware-specific part of the product configuration
+$(call inherit-product, device/oukitel/k6000_plus/device.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := K5000
+
+PRODUCT_DEVICE := k5000
+PRODUCT_NAME := full_k5000
+PRODUCT_BRAND := Oukitel
+PRODUCT_MODEL := K5000
+PRODUCT_MANUFACTURER := Oukitel
+PRODUCT_RESTRICT_VENDOR_FILES := false
+
+# Boot animation
+TARGET_SCREEN_HEIGHT      := 1280
+TARGET_SCREEN_WIDTH       := 720
+TARGET_BOOTANIMATION_NAME := 720
+
+PRODUCT_DEFAULT_LANGUAGE := en
+PRODUCT_DEFAULT_REGION   := US
